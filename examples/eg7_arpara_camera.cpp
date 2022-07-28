4587d2a77ea95b4c29a15f28761e1c820b0437ab
hanbowen <fancycode@gmail.com>
2022-07-28 16:54:55 +0800

    Fix a crash when there is no arapra camera

--- examples/eg7_arpara_camera.cpp
+++ examples/eg7_arpara_camera.cpp
@@ -16,54 +16,61 @@ void eg7_arpara_camera()
     for (int i = 0; i < cameraDeivceList.size(); i++) {
         std::cout << cameraDeivceList[i] << std::endl;
     }
-
-    // Get first support resolution in the first camera
-    std::vector <std::pair<int, int>> resolutions = cameraDeivceList[0].getResolutions();
-    std::cout << "Width: " + std::to_string(resolutions[0].first) + ", Height: " + std::to_string(resolutions[0].second) << std::endl;
-     
-
-
-    // Open the first camera in the biggest resolution
-    std::cout << "Open the first camera..." << std::endl;
-    camera.open(cameraDeivceList[0], 
-        resolutions[resolutions.size() - 1].first,
-        resolutions[resolutions.size() - 1].second
-    );
-    std::cout << "show DirectShow properties" << std::string(*camera.getDirectShowProperties()) << std::endl;
-    // Get exposure in second
-    double exposureTime = camera.getExposure();
-    if (exposureTime > 0.0)
+    if (cameraDeivceList.size() == 0)
     {
-        std::cout << "Exposure: " + std::to_string(exposureTime) + "s" << std::endl;
-        // Set exposure as the largest value
-        std::vector<double> exposures = camera.getPossibleExposureValues();
-        camera.setExposure(exposures[exposures.size() - 1]);
-        std::cout << "Set exposure to " + std::to_string(exposures[exposures.size() - 1]) + "s" << std::endl;
-
-    }else
+        std::cout << "no arpara camera found, exiting" << std::endl;
+    } else
     {
-        std::cout << "This device doesn't support exposure time" << std::endl;
-    }
+        // Get first support resolution in the first camera
+        std::vector <std::pair<int, int>> resolutions = cameraDeivceList[0].getResolutions();
+        std::cout << "Width: " + std::to_string(resolutions[0].first) + ", Height: " + std::to_string(resolutions[0].second)  << std::endl;
 
 
-    // Reset all properties to default
-    std::cout << "Reset properties to default..." << std::endl;
-    //camera.resetProperties();
-    {
-        camera.startCapture();
-        // Wait for 1 second
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-        cv::Mat frame;
-        while(true)
+
+        // Open the first camera in the biggest resolution
+        std::cout << "Open the first camera..." << std::endl;
+        camera.open(cameraDeivceList[0],
+            resolutions[resolutions.size() - 1].first,
+            resolutions[resolutions.size() - 1].second
+        );
+        std::cout << "show DirectShow properties" << std::string(*camera.getDirectShowProperties()) << std::endl;
+        // Get exposure in second
+        double exposureTime = camera.getExposure();
+        if (exposureTime > 0.0)
         {
-            frame = camera.getMat();
-            cv::imshow("frame", frame);
-            if (cv::waitKey(1) == 'q')
-			{
-				break;
-			}
+            std::cout << "Exposure: " + std::to_string(exposureTime) + "s" << std::endl;
+            // Set exposure as the largest value
+            std::vector<double> exposures = camera.getPossibleExposureValues();
+            camera.setExposure(exposures[exposures.size() - 1]);
+            std::cout << "Set exposure to " + std::to_string(exposures[exposures.size() - 1]) + "s" << std::endl;
+
+        }
+        else
+        {
+            std::cout << "This device doesn't support exposure time" << std::endl;
+        }
+
+
+        // Reset all properties to default
+        std::cout << "Reset properties to default..." << std::endl;
+        //camera.resetProperties();
+        {
+            camera.startCapture();
+            // Wait for 1 second
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            cv::Mat frame;
+            while (true)
+            {
+                frame = camera.getMat();
+                cv::imshow("frame", frame);
+                if (cv::waitKey(1) == 'q')
+                {
+                    break;
+                }
+            }
         }
     }
+
     camera.stopCapture();
     // Close the camera.
     camera.close();
